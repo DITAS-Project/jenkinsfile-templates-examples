209b358102794d4e3553e805e9b735d046a7ee1c
Borja Tornos <btornos@ideko.es>
2019-02-01 14:18:45 +0100

    Added production deployment script

--- main-example/jenkins/deploy/deploy-production.sh
+++ main-example/jenkins/deploy/deploy-production.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# Staging environment: 31.171.247.162
+# Production environment: 178.22.69.83
 # Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem
 
 # TODO state management? We are killing without careing about any operation the conainer could be doing.
 
-ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
+ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@178.22.69.83 << 'ENDSSH'
 # Ensure that a previously running instance is stopped (-f stops and removes in a single step)
 # || true - "docker stop" fails with exit status 1 if image doen't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0
 # Try a graceful stop: 20 seconds for SIGTERM and SIGKILL after that
