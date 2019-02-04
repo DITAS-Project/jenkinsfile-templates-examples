cc730f595e96baaf640f039c907d2828a15517a3
borjakhet <btornos@ideko.es>
2019-02-04 11:44:51 +0100

    Update run-api-test.sh

--- main-example/jenkins/dredd/run-api-test.sh
+++ main-example/jenkins/dredd/run-api-test.sh
@@ -3,14 +3,13 @@
 # This script will try to connect to your component during 3 minutes
 
 # TO CHANGE: This line should point to your component in Staging
-COMPONENT_ENDPOINT="http://31.171.247.162:50011"
+COMPONENT_URL="http://31.171.247.162:50011"
 
 # TO CHANGE: This line should point to the yaml file of your respository of your api definition
 YAML_FILE="a.yaml"
 
 attempts=0
-
-until $(curl --output /dev/null --silent --head --fail $COMPONENT_ENDPOINT); do
+until $(curl --output /dev/null --silent --head --fail $COMPONENT_URL); do
     if [ ${attempts} -eq 20 ];then
       echo "Max attempts reached"
       exit 1
@@ -20,5 +19,6 @@ until $(curl --output /dev/null --silent --head --fail $COMPONENT_ENDPOINT); do
     attempts=$(($attempts+1))
     sleep 9
 done
+
 # If you use Hooks, you have to modify this line. Read the Dredd manual.
-dredd $YAML_FILE $COMPONENT_ENDPOINT
+dredd $YAML_FILE $COMPONENT_URL
